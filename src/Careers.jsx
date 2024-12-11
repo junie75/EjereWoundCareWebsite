851413f8a49a9013d69e4e5dd10c00d96d5f1dcf
junie75 <juniejere6654@gmail.com>
2024-12-11 09:26:32 -0600

    added structured schema data with detailed medicalclinic info to index.html so that search engines can crawl and receive more information. I fixed the schema data in careers.jsx because it would only be added to the html when the modal is clicked. Instead I display it with each job posting

--- src/Careers.jsx
+++ src/Careers.jsx
@@ -7,6 +7,69 @@ import Markdown from "react-markdown";
 
 // Modal component to display job details
 const JobModal = ({ job, onClose }) => {
+  // // Utility function to strip markdown syntax
+  // const stripMarkdown = (markdownText) => {
+  //   return markdownText
+  //     .replace(/!\[.*\]\(.*\)/g, "") // Remove images
+  //     .replace(/\[([^\]]+)\]\([^)]+\)/g, "$1") // Convert links to text
+  //     .replace(/[#>*_`~\-]/g, "") // Remove markdown characters
+  //     .replace(/\r?\n|\r/g, " ") // Replace line breaks with spaces
+  //     .trim();
+  // };
+
+  // // Convert markdown content to plain text for the description
+  // const plainTextContent = stripMarkdown(job.content);
+
+  // Define JSON-LD schema data for the job posting to be available to search engines
+  // const schemaData = {
+  //   "@context": "http://schema.org",
+  //   "@type": "JobPosting",
+  //   title: job.jobTitle,
+  //   description: plainTextContent,
+  //   hiringOrganization: {
+  //     "@type": "MedicalClinic",
+  //     name: "Ejere Wound Care and Hyperbaric Oxygen Therapy",
+  //     url: "https://ejerewoundcare.com",
+  //     logo: "https://www.ejerewoundcare.com/assets/RedoneLogoTrans400x118-Z3vkd7aU.png",
+  //   },
+  //   employmentType: "FULL-TIME" /*job.jobType.toUpperCase(),*/,
+  //   jobLocation: {
+  //     "@type": "Place",
+  //     address: {
+  //       "@type": "PostalAddress",
+  //       addressLocality: "Weatherford",
+  //       addressRegion: "TX",
+  //       addressCountry: "US",
+  //       postalCode: "76086",
+  //     },
+  //   },
+  //   datePosted: "2024-11-11",
+  //   validThrough: "2024-12-31", //expiration date
+  //   applyLink: job.applyLink,
+  // };
+
+  return (
+    <div className="modal" key={job.jobID}>
+      <div className="modalContent">
+        <div className="close" onClick={onClose}>
+          &times;
+        </div>
+        {/* Render Job content after parsing markdown */}
+        <Markdown>{job.content}</Markdown>
+        {/* Render JSON-LD schema data as a script tag to be crawled by search engines */}
+        {/* <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
+        /> */}
+      </div>
+    </div>
+  );
+};
+
+export default function Careers() {
+  const [modalJob, setModalJob] = useState(null); // State to manage the modal
+  const jobPostings = getJobPostings(); // Get job postings from the getJobPostings function
+
   // Utility function to strip markdown syntax
   const stripMarkdown = (markdownText) => {
     return markdownText
@@ -18,56 +81,7 @@ const JobModal = ({ job, onClose }) => {
   };
 
   // Convert markdown content to plain text for the description
-  const plainTextContent = stripMarkdown(job.content);
-
-  // Define JSON-LD schema data for the job posting to be available to search engines
-  const schemaData = {
-    "@context": "http://schema.org",
-    "@type": "JobPosting",
-    title: job.jobTitle,
-    description: plainTextContent,
-    hiringOrganization: {
-      "@type": "Organization",
-      name: "Ejere Wound Care and Hyperbaric Oxygen Therapy",
-      url: "https://ejerewoundcare.com",
-      logo: "https://ejerewoundcare.com/assets/RedoneLogoTrans400x118-Z3vkd7aU.png", // Update with actual logo URL
-    },
-    employmentType: "FULL_TIME" /*job.jobType.toUpperCase(),*/,
-    jobLocation: {
-      "@type": "Place",
-      address: {
-        "@type": "PostalAddress",
-        addressLocality: "Weatherford", // Update with actual city
-        addressRegion: "TX", // Update with actual state
-        addressCountry: "US", // Update with actual country
-      },
-    },
-    datePosted: "2024-11-11",
-    validThrough: "2024-12-31", //expiration date
-    applyLink: job.applyLink,
-  };
-
-  return (
-    <div className="modal" key={job.jobID}>
-      <div className="modalContent">
-        <div className="close" onClick={onClose}>
-          &times;
-        </div>
-        {/* Render Job content after parsing markdown */}
-        <Markdown>{job.content}</Markdown>
-        {/* Render JSON-LD schema data as a script tag to be crawled by search engines */}
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
-        />
-      </div>
-    </div>
-  );
-};
-
-export default function Careers() {
-  const [modalJob, setModalJob] = useState(null); // State to manage the modal
-  const jobPostings = getJobPostings(); // Get job postings from the getJobPostings function
+  // const plainTextContent = stripMarkdown(job.content);
   return (
     <Layout>
       <div className="pageContainer">
@@ -88,40 +102,83 @@ export default function Careers() {
                 No job postings at this time
               </div>
             ) : (
-              jobPostings.map((post) => (
-                <div
-                  className="job"
-                  key={post.jobID}
-                  onClick={() => setModalJob(post)} // Open the modal when a job is clicked
-                >
-                  <section>
-                    <h2 className="jobTitle crimson-pro-bold">
-                      {post.jobTitle}
-                    </h2>
-                    <div
-                      className="apply crimson-pro-reg"
-                      onClick={() => setModalJob(post)}
-                    >
-                      <h2>Learn More</h2>
+              jobPostings.map((post) => {
+                const plainTextContent = stripMarkdown(post.content);
+                const schemaData = {
+                  "@context": "http://schema.org",
+                  "@type": "JobPosting",
+                  title: post.jobTitle,
+                  description: plainTextContent,
+                  hiringOrganization: {
+                    "@type": "Organization",
+                    name: "Ejere Wound Care and Hyperbaric Oxygen Therapy",
+                    url: "https://ejerewoundcare.com",
+                    logo: "https://www.ejerewoundcare.com/assets/RedoneLogoTrans400x118-Z3vkd7aU.png",
+                    address: {
+                      "@type": "PostalAddress",
+                      streetAddress: "730 E Eureka St.",
+                      addressLocality: "Weatherford",
+                      addressRegion: "TX",
+                      postalCode: "76087",
+                      addressCountry: "US",
+                    },
+                  },
+                  employmentType: "FULL-TIME" /*job.jobType.toUpperCase(),*/,
+                  jobLocation: {
+                    "@type": "Place",
+                    address: {
+                      "@type": "PostalAddress",
+                      addressLocality: "Weatherford",
+                      addressRegion: "TX",
+                      addressCountry: "US",
+                      postalCode: "76086",
+                    },
+                  },
+                  datePosted: "2024-11-11",
+                  validThrough: "2024-12-31", //expiration date
+                  applyLink: post.applyLink,
+                };
+                return (
+                  <div
+                    className="job"
+                    key={post.jobID}
+                    onClick={() => setModalJob(post)} // Open the modal when a job is clicked
+                  >
+                    <section>
+                      <h2 className="jobTitle crimson-pro-bold">
+                        {post.jobTitle}
+                      </h2>
+                      <div
+                        className="apply crimson-pro-reg"
+                        onClick={() => setModalJob(post)}
+                      >
+                        <h2>Learn More</h2>
 
-                      <span className="material-symbols-outlined icon-medium">
-                        north_east
-                      </span>
+                        <span className="material-symbols-outlined icon-medium">
+                          north_east
+                        </span>
+                      </div>
+                    </section>
+                    <div className="jobDesc crimson-pro-reg">
+                      <p>{post.jobDesc}</p>
                     </div>
-                  </section>
-                  <div className="jobDesc crimson-pro-reg">
-                    <p>{post.jobDesc}</p>
+                    <div className="jobCharacteristics">
+                      <div className="jobType crimson-pro-reg">
+                        {post.jobType}
+                      </div>
+                      <div className="jobType crimson-pro-reg">
+                        {post.jobLocation}
+                      </div>
+                    </div>
+                    <script
+                      type="application/ld+json"
+                      dangerouslySetInnerHTML={{
+                        __html: JSON.stringify(schemaData),
+                      }}
+                    />
                   </div>
-                  <div className="jobCharacteristics">
-                    <div className="jobType crimson-pro-reg">
-                      {post.jobType}
-                    </div>
-                    <div className="jobType crimson-pro-reg">
-                      {post.jobLocation}
-                    </div>
-                  </div>
-                </div>
-              ))
+                );
+              })
             )
           }
           {
